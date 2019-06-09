db1750c6fd56d4ab7a448e5a770b607dc6c94234
Ragnor Comerford <hello@ragnor.co>
2019-06-09 22:30:32 +0200

    Fix tracking.

--- files/js/script.js
+++ files/js/script.js
@@ -453,11 +453,4 @@ var siriWave = new SiriWave({
 	  siriWave.setAmplitude(1);
 	}, 1000);
 	
-	(function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:1107668,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+	
