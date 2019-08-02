b1cbc42c19a1f9013f7ecdb955f4a64449f4d8d1
Ragnor Comerford <hello@ragnor.co>
2019-08-02 20:32:37 +0200

    Add initial menu

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="layout text-red-800">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metaData.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about">About</g-link>
-      </nav>
+  <div class="px-32">
+    <header>
+      <div class="flex h-20 items-center justify-between">
+        <img class="h-10" src="../assets/img/logos/rc.png" />
+        <nav>
+          <MenuIcon @click="toggle" size="1.5x" class="absolute right-0 mr-10 lg:hidden"></MenuIcon>
+          <div
+            class="flex items-center justify-between ragtrans"
+            :class="open ? 'flex-col absolute right-0 mr-10 mt-20': 'flex-row sm:hidden lg:flex'"
+          >
+            <g-link class="nav__link" to="/">Home</g-link>
+            <g-link class="nav__link" to="/about">About</g-link>
+          </div>
+        </nav>
+      </div>
     </header>
     <slot />
   </div>
@@ -20,6 +26,26 @@ query {
   }
 }
 </static-query>
+
+<script>
+import { MenuIcon } from "vue-feather-icons";
+export default {
+  name: "NavBar",
+  components: {
+    MenuIcon
+  },
+  data() {
+    return {
+      open: false
+    };
+  },
+  methods: {
+    toggle() {
+      this.open = !this.open;
+    }
+  }
+};
+</script>
 
 <style>
 body {
@@ -35,14 +61,6 @@ body {
   margin: 0 auto;
   padding-left: 20px;
   padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
 }
 
 .nav__link {
