77eba5d3bb4c2178f15ec0491f1ce8ea4081f585
Ragnor Comerford <hello@ragnor.co>
2019-08-03 18:19:49 +0200

    Make collections responsive.

--- src/pages/Collections.vue
+++ src/pages/Collections.vue
@@ -5,10 +5,10 @@
         <h1 class="text-5xl font-bold mb-12">
           <span class="text-gold">C</span>ollections
         </h1>
-        <div class="flex flex-row">
+        <div class="hidden md:flex lg:flex xl:flex">
           <div class="flex flex-col w-1/3">
             <div
-              class="flex h-96 relative rounded-lg reading bg-center bg-no-repeat bg-contain justify-center items-start mr-8 mb-8"
+              class="flex h-64 lg:h-96 relative rounded-lg reading bg-center bg-no-repeat bg-contain justify-center items-start mr-8 mb-8"
             >
               <img src="~@/assets/img/illustrations/books.svg" class="w-2/3 mt-16" />
               <h2 class="absolute left-0 bottom-0 m-6 text-xl">Books</h2>
@@ -17,13 +17,13 @@
           <div class="flex flex-col w-2/3">
             <div class="flex flex-row">
               <div
-                class="flex w-1/3 h-64 relative rounded-lg reading bg-center bg-no-repeat bg-contain justify-end items-center mr-8 mb-8"
+                class="flex w-1/3 h-48 lg:h-64 relative rounded-lg reading bg-center bg-no-repeat bg-contain justify-end items-center mr-8 mb-8"
               >
                 <img src="~@/assets/img/illustrations/people.svg" class="w-2/3 mr-5" />
                 <h2 class="absolute left-0 bottom-0 m-6 text-xl">People</h2>
               </div>
               <div
-                class="flex w-2/3 h-64 relative rounded-lg reading bg-center bg-no-repeat bg-contain justify-end items-center mr-8 mb-8"
+                class="flex w-2/3 h-48 lg:h-64 relative rounded-lg reading bg-center bg-no-repeat bg-contain justify-end items-center mr-8 mb-8"
               >
                 <img src="~@/assets/img/illustrations/watching.svg" class="w-2/3 mr-8" />
                 <h2 class="absolute left-0 bottom-0 m-6 text-xl">Movies and Series</h2>
@@ -31,12 +31,32 @@
             </div>
             <div class="flex flex-row">
               <div
-                class="flex w-2/3 h-64 relative rounded-lg reading bg-center bg-no-repeat bg-contain justify-end items-center mr-8 mb-8"
+                class="flex w-2/3 h-48 lg:h-64 relative rounded-lg reading bg-center bg-no-repeat bg-contain justify-end items-center mr-8 mb-8"
               >
                 <img src="~@/assets/img/illustrations/tools.svg" class="w-1/2 mr-5" />
                 <h2 class="absolute left-0 bottom-0 m-6 text-xl">Tools and Services</h2>
               </div>
             </div>
+          </div>
+        </div>
+        <!--  Mobile Version -->
+
+        <div class="flex md:hidden lg:hidden xl:hidden flex-col">
+          <div class="flex h-48 relative rounded-lg reading justify-end items-center mr-8 mb-8">
+            <img src="~@/assets/img/illustrations/books.svg" class="h-32 mr-8" />
+            <h2 class="absolute left-0 bottom-0 m-6 text-xl">Books</h2>
+          </div>
+          <div class="flex h-48 relative rounded-lg reading justify-end items-center mr-8 mb-8">
+            <img src="~@/assets/img/illustrations/tools.svg" class="h-24 mr-8" />
+            <h2 class="absolute left-0 bottom-0 m-6 text-xl">Tools and Services</h2>
+          </div>
+          <div class="flex h-48 relative rounded-lg reading justify-end items-center mr-8 mb-8">
+            <img src="~@/assets/img/illustrations/people.svg" class="h-32 mr-8" />
+            <h2 class="absolute left-0 bottom-0 m-6 text-xl">People</h2>
+          </div>
+          <div class="flex h-48 relative rounded-lg reading justify-end items-center mr-8 mb-8">
+            <img src="~@/assets/img/illustrations/watching.svg" class="h-24 mr-8" />
+            <h2 class="absolute left-0 bottom-0 m-6 text-xl">Movies and Series</h2>
           </div>
         </div>
       </div>
