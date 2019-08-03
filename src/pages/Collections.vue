fc022633b4f6b2233b1d17b2b19ee0c57fcd65cf
Ragnor Comerford <hello@ragnor.co>
2019-08-03 17:55:57 +0200

    Add grid.

--- src/pages/Collections.vue
+++ src/pages/Collections.vue
@@ -10,7 +10,7 @@
             <div
               class="flex h-96 relative rounded-lg reading bg-center bg-no-repeat bg-contain justify-center items-start mr-8 mb-8"
             >
-              <img src="~@/assets/img/illustrations/books.svg" class="h-48 mt-16" />
+              <img src="~@/assets/img/illustrations/books.svg" class="w-2/3 mt-16" />
               <h2 class="absolute left-0 bottom-0 m-6 text-xl">Books</h2>
             </div>
           </div>
@@ -19,21 +19,21 @@
               <div
                 class="flex w-1/3 h-64 relative rounded-lg reading bg-center bg-no-repeat bg-contain justify-end items-center mr-8 mb-8"
               >
-                <img src="~@/assets/img/illustrations/people.svg" class="h-32 mr-5" />
+                <img src="~@/assets/img/illustrations/people.svg" class="w-2/3 mr-5" />
                 <h2 class="absolute left-0 bottom-0 m-6 text-xl">People</h2>
               </div>
               <div
                 class="flex w-2/3 h-64 relative rounded-lg reading bg-center bg-no-repeat bg-contain justify-end items-center mr-8 mb-8"
               >
-                <img src="~@/assets/img/illustrations/watching.svg" class="h-40 mr-8" />
+                <img src="~@/assets/img/illustrations/watching.svg" class="w-2/3 mr-8" />
                 <h2 class="absolute left-0 bottom-0 m-6 text-xl">Movies and Series</h2>
               </div>
             </div>
             <div class="flex flex-row">
               <div
-                class="flex w-3/4 h-64 relative rounded-lg reading bg-center bg-no-repeat bg-contain justify-end items-center mr-8 mb-8"
+                class="flex w-2/3 h-64 relative rounded-lg reading bg-center bg-no-repeat bg-contain justify-end items-center mr-8 mb-8"
               >
-                <img src="~@/assets/img/illustrations/tools.svg" class="h-40 mr-5" />
+                <img src="~@/assets/img/illustrations/tools.svg" class="w-1/2 mr-5" />
                 <h2 class="absolute left-0 bottom-0 m-6 text-xl">Tools and Services</h2>
               </div>
             </div>
