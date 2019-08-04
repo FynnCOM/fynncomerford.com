bacb73e614cfc79b4c2458c16534b1cd51892cb1
Ragnor Comerford <hello@ragnor.co>
2019-08-04 15:05:52 +0200

    FIx shadow.

--- src/pages/Collections.vue
+++ src/pages/Collections.vue
@@ -8,7 +8,7 @@
         <div class="hidden md:flex lg:flex xl:flex">
           <div class="flex flex-col w-1/3">
             <div
-              class="flex h-64 lg:h-96 relative rounded-lg reading bg-center bg-no-repeat bg-contain justify-center items-start mr-8 mb-8"
+              class="flex h-64 lg:h-96 relative rounded-lg shadow bg-center bg-no-repeat bg-contain justify-center items-start mr-8 mb-8"
             >
               <img src="~@/assets/img/illustrations/books.svg" class="w-2/3 mt-16" />
               <h2 class="absolute left-0 bottom-0 m-6 text-xl">Books</h2>
@@ -17,13 +17,13 @@
           <div class="flex flex-col w-2/3">
             <div class="flex flex-row">
               <div
-                class="flex w-1/3 h-48 lg:h-64 relative rounded-lg reading bg-center bg-no-repeat bg-contain justify-end items-center mr-8 mb-8"
+                class="flex w-1/3 h-48 lg:h-64 relative rounded-lg shadow bg-center bg-no-repeat bg-contain justify-end items-center mr-8 mb-8"
               >
                 <img src="~@/assets/img/illustrations/people.svg" class="w-2/3 mr-5" />
                 <h2 class="absolute left-0 bottom-0 m-6 text-xl">People</h2>
               </div>
               <div
-                class="flex w-2/3 h-48 lg:h-64 relative rounded-lg reading bg-center bg-no-repeat bg-contain justify-end items-center mr-8 mb-8"
+                class="flex w-2/3 h-48 lg:h-64 relative rounded-lg shadow bg-center bg-no-repeat bg-contain justify-end items-center mr-8 mb-8"
               >
                 <img src="~@/assets/img/illustrations/watching.svg" class="w-2/3 mr-8" />
                 <h2 class="absolute left-0 bottom-0 m-6 text-xl">Movies and Series</h2>
@@ -31,7 +31,7 @@
             </div>
             <div class="flex flex-row">
               <div
-                class="flex w-2/3 h-48 lg:h-64 relative rounded-lg reading bg-center bg-no-repeat bg-contain justify-end items-center mr-8 mb-8"
+                class="flex w-2/3 h-48 lg:h-64 relative rounded-lg shadow bg-center bg-no-repeat bg-contain justify-end items-center mr-8 mb-8"
               >
                 <img src="~@/assets/img/illustrations/tools.svg" class="w-1/2 mr-5" />
                 <h2 class="absolute left-0 bottom-0 m-6 text-xl">Tools and Services</h2>
@@ -42,19 +42,19 @@
         <!--  Mobile Version -->
 
         <div class="flex md:hidden lg:hidden xl:hidden flex-col">
-          <div class="flex h-48 relative rounded-lg reading justify-end items-center mr-8 mb-8">
+          <div class="flex h-48 relative rounded-lg shadow justify-end items-center mr-8 mb-8">
             <img src="~@/assets/img/illustrations/books.svg" class="h-32 mr-8" />
             <h2 class="absolute left-0 bottom-0 m-6 text-xl">Books</h2>
           </div>
-          <div class="flex h-48 relative rounded-lg reading justify-end items-center mr-8 mb-8">
+          <div class="flex h-48 relative rounded-lg shadow justify-end items-center mr-8 mb-8">
             <img src="~@/assets/img/illustrations/tools.svg" class="h-24 mr-8" />
             <h2 class="absolute left-0 bottom-0 m-6 text-xl">Tools and Services</h2>
           </div>
-          <div class="flex h-48 relative rounded-lg reading justify-end items-center mr-8 mb-8">
+          <div class="flex h-48 relative rounded-lg shadow justify-end items-center mr-8 mb-8">
             <img src="~@/assets/img/illustrations/people.svg" class="h-32 mr-8" />
             <h2 class="absolute left-0 bottom-0 m-6 text-xl">People</h2>
           </div>
-          <div class="flex h-48 relative rounded-lg reading justify-end items-center mr-8 mb-8">
+          <div class="flex h-48 relative rounded-lg shadow justify-end items-center mr-8 mb-8">
             <img src="~@/assets/img/illustrations/watching.svg" class="h-24 mr-8" />
             <h2 class="absolute left-0 bottom-0 m-6 text-xl">Movies and Series</h2>
           </div>
@@ -71,9 +71,3 @@ export default {
   }
 };
 </script>
-
-<style>
-.reading {
-  box-shadow: 0px 0px 45px 0px rgba(0, 0, 0, 0.15);
-}
-</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -187,8 +187,7 @@ module.exports = {
       "8": "8px"
     },
     boxShadow: {
-      default:
-        "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
+      default: "0px 0px 45px 0px rgba(0, 0, 0, 0.15)",
       md:
         "0 4px 6px 1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
       lg:
