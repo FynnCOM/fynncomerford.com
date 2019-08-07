0622d1330116f708cf53a9d25520065ac6969ab7
Ragnor Comerford <hello@ragnor.co>
2019-08-07 14:37:32 +0200

    Fix header.

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="h-full w-full relative">
     <ClientOnly>
-      <div class="opacity-25 absolute h-24 top-0 right-0 mt-20 w-full z-20" id="wave"></div>
+      <div
+        :class="open ? 'block': 'hidden'"
+        class="opacity-25 absolute h-24 top-0 right-0 mt-20 w-full z-20"
+        id="wave"
+      ></div>
     </ClientOnly>
     <vue-progress-bar></vue-progress-bar>
-    <MenuIcon @click="toggle" size="1.5x" class="absolute right-0 mr-10 mt-10 lg:hidden xl:hidden"></MenuIcon>
+    <MenuIcon
+      @click="toggle"
+      size="1.5x"
+      class="absolute right-0 mr-10 mt-10 lg:hidden xl:hidden z-30"
+    ></MenuIcon>
 
     <div class="px-16">
       <header>
@@ -33,17 +41,21 @@
       <slot />
     </div>
     <div
-      :class="open ? 'w-full': 'w-0'"
-      class="h-screen bg-white shadow-lg overflow-auto float-right absolute top-0 right-0 transition-w transition-500"
+      :class="open ? 'block opacity-100': 'hidden opacity-0'"
+      class="h-screen bg-white overflow-auto float-right absolute top-0 right-0 transition-opacity transition-1000 w-full z-40"
     >
       <XIcon @click="toggle" size="1.5x" class="mt-8 mr-8 float-right"></XIcon>
       <div class="flex flex-col items-end mt-20 mr-8">
-        <g-link active-class="text-gold" to="/">Home</g-link>
+        <g-link active-class="text-gold" to="/" exact>Home</g-link>
         <g-link active-class="text-gold" to="/about">About</g-link>
-        <g-link active-class="text-gold" to="/about">Projects</g-link>
-        <g-link active-class="text-gold" to="/about">Collections</g-link>
-        <g-link active-class="text-gold" to="/about">CV</g-link>
-        <g-link active-class="text-gold" to="/about">Thoughts</g-link>
+        <g-link active-class="text-gold" to="/projects">Projects</g-link>
+        <g-link active-class="text-gold" to="/collections">Collections</g-link>
+        <a
+          target="_blank"
+          class="ml-5 hover:text-gold"
+          href="https://drive.google.com/file/d/1XddOM9HOTuT6uIM2cLGCYK5cK_PNSdJA/view?usp=sharing"
+        >CV</a>
+        <g-link active-class="text-gold" to="/thoughts">Thoughts</g-link>
       </div>
     </div>
   </div>
@@ -97,9 +109,5 @@ export default {
 };
 </script>
 
-<style>
-.ragtrans {
-  transition-property: width;
-  transition-duration: 0.5s;
-}
+<style scoped>
 </style>
