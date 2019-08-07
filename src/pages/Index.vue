659f4e6b0791b97d8e1a18e8ec4f1c935acfa276
Ragnor Comerford <hello@ragnor.co>
2019-08-07 21:15:47 +0200

    Try fix.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,34 +3,8 @@
     <div class="pl-18 lg:pl-32 pt-24 w-2/3 leading-relaxed">
       <p class="mt-6">
         I’m Ragnor. In case you don’t know me yet, feel free to check
-        my
-        <a
-          style="color: black; text-decoration: underline;"
-          href="about.html"
-        >about</a> page.
-        <br />Here, I occasionally write about I am working on and
-        <a
-          style="color: black; text-decoration: underline;"
-          href="https://ragnorcomerford.com/thoughts/"
-        >thoughts and ideas</a>&nbsp;I have on
-        different&nbsp;topics.
-        <br />I also compile a
-        <a
-          style="color: black; text-decoration: underline;"
-          href="https://ragnorcomerford.com/collections/"
-        >collection</a> of books, movies, and
-        articles I particularly like and recommend.
-        <br />If you have an interesting project or just
-        want to grab a coffee and chat, don’t hesitate to
-        <a
-          style="color: black; text-decoration: underline;"
-          href="mailto:hello@ragnor.co"
-        >
-          drop me
-          a message
-        </a>.
+        mypage.
       </p>
-      <img src="@/assets/img/signature.png" class="h-24 mt-8" />
     </div>
   </Layout>
 </template>
